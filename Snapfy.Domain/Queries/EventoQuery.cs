170d00cdbed34a1218cfeceb0b72092df8a86001
Gustavo Begnini <gustavo@digibyte.com.br>
2021-10-27 10:30:15 -0300

    Correção Datas

--- Snapfy.Domain/Queries/EventoQuery.cs
+++ Snapfy.Domain/Queries/EventoQuery.cs
@@ -15,16 +15,16 @@ namespace Shoalace.Domain.Queries
             e => e.MembrosEvento != null && e.MembrosEvento.Any(m => m.UsuarioId == usuarioId);
 
         public static Expression<Func<Evento, bool>> ObterPor2Usuarios(long usuarioId, long contatoId) =>
-            e => e.MembrosEvento != null && e.GrupoId == null && e.Data >= DateTime.Now.AddHours(-5) && e.MembrosEvento.Count() == 2 && e.MembrosEvento.Any(m => m.UsuarioId == contatoId) && e.MembrosEvento.Any(m => m.UsuarioId == usuarioId);
+            e => e.MembrosEvento != null && e.GrupoId == null && e.Data.Date >= DateTime.Now.Date && e.MembrosEvento.Count() == 2 && e.MembrosEvento.Any(m => m.UsuarioId == contatoId) && e.MembrosEvento.Any(m => m.UsuarioId == usuarioId);
 
         public static Expression<Func<Evento, bool>> ObterProximosPorUsuario(long usuarioId) =>
             e => e.Data >= DateTime.Now.AddHours(-5) && e.MembrosEvento != null && e.MembrosEvento.Any(m => m.UsuarioId == usuarioId);
 
         public static Expression<Func<Evento, bool>> ObterTodosExplorar() =>
-           e => e.Categoria != ECategoria.Privado && e.Data >= DateTime.Now;
+           e => e.Categoria != ECategoria.Privado && e.Data.Date >= DateTime.Now.Date;
 
         public static Expression<Func<Evento, bool>> ObterTodosPorData(DateTime data) =>
-            e => e.Data == data;
+            e => e.Data.Date == DateTime.Now.Date;
 
         public static Expression<Func<Evento, bool>> ObterPorCategoriaECidade(ECategoria categoria, string cidade) =>
             e => (e.Categoria == categoria || categoria == ECategoria.Publico) && (e.Cidade == cidade || cidade == "");
